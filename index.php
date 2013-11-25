38752c84a405a5c24d1ef0540ea89d332efa6c51
hequet <plhequet@numericable.fr>
2013-11-25 11:31:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 
-
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 	<head>
 		 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
-		 <meta author="Pierrard et Hequet" />
+		 <meta author="Pierard, Hequet" />
 		 <meta name="keywords" content="" />
 		 <meta name="description" content="" />
 		 <title>CasLepino</title>
